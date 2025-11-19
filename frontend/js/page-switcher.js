1dbdbdafef14d8cadd1783ac659322b4346dca7d
porcielf-eagle <microsoft-secondario.deviate737@passmail.net>
2025-11-19 15:59:03 +0100

    Ended the animation for the switch between the quiz and the login page. Now time to handle the leaderboard.

--- frontend/js/page-switcher.js
+++ frontend/js/page-switcher.js
@@ -1,32 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
+function    switchPage(originPage, targetPage) {
     const   loginPageBorder = document.getElementById("login-page-border");
     const   quizPageBorder = document.getElementById("quiz-page-border");
     const   quizBackground = document.getElementById("quiz-background");
     const   loginPage = document.getElementById("login-page");
 
-    const   pageLinks = document.querySelectorAll("a[data-page]");
-
-    function    switchPage(targetPage) {
-        if (targetPage === "quiz") {
-            loginPageBorder.style.display = "none";
-            quizPageBorder.style.display = "flex";
-        } else if (targetPage === "login") {
-            quizPageBorder.style.display = "none";
-            loginPageBorder.style.display = "flex";
-
-            loginPageBorder.style.animationName = "border-from-quiz-to-login";
-            loginPage.style.animationName = "from-quiz-to-login";
-
-            quizBackground.style.visibility = "visible";
-            quizBackground.style.animationPlayState = "running";
-        }
+    if (originPage === "login" && targetPage === "quiz") {
+        loginPageBorder.style.display = "none";
+        quizPageBorder.style.display = "flex";
+    } else if (originPage === "quiz" && targetPage === "login") {
+        quizPageBorder.style.display = "none";
+        loginPageBorder.style.display = "flex";
+        loginPageBorder.style.animationName = "border-from-quiz-to-login"
+        loginPage.style.animationName = "from-quiz-to-login";
+        quizBackground.style.visibility = "visible";
+        quizBackground.style.animationPlayState = "running";
     }
-
-    pageLinks.forEach(link => {
-        link.addEventListener("click", (e) => {
-            e.preventDefault();
-            const   targetPage = link.getAttribute("data-page");
-            switchPage(targetPage);
-        });
-    });
-});
+}
